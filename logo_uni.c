3ac7ef94a8ace13791896c55b04bf8987f6ae314
Danitrix13 <138601001+Danitrix13@users.noreply.github.com>
2023-08-21 18:57:46 -0600

    trabajo logo uni del segundo semestre

---
+++ logo_uni.c
@@ -0,0 +1,292 @@
+#include <conio.h>
+
+void main()
+{   int i;
+	clrscr();
+	for (i = 0; i <2080; i++)
+	{
+		if (i > 498 && i < 506)
+		{
+			gotoxy(0, 0);
+			textcolor(BLUE);
+			cprintf("%c", 219);
+		}
+		else if (i > 507 && i < 534)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 535 && i < 544)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 578 && i < 586)
+		{
+			gotoxy(0, 0);
+			textcolor(BLUE);
+			cprintf("%c", 219);
+		}
+		
+		else if (i > 587 && i < 614)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 615 && i < 624)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 658 && i < 666)
+		{
+			gotoxy(0, 0);
+			textcolor(BLUE);
+			cprintf("%c", 219);
+		}
+		else if (i > 667 && i < 694)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 695 && i < 704)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 738 && i < 746)
+		{
+			gotoxy(0, 0);
+			textcolor(BLUE);
+			cprintf("%c", 219);
+		}
+		else if (i > 747 && i < 774)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 775 && i < 784)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+
+		else if (i > 818 && i < 826)
+		{
+			gotoxy(0, 0);
+			textcolor(BLUE);
+			cprintf("%c", 219);
+		}
+		else if (i > 827 && i < 835)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 845 && i < 854)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 898 && i < 906)
+		{	gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+		}
+		else if (i > 907 && i < 915)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 916 && i < 924)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 925 && i < 934)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 935 && i < 944)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		
+		else if(i > 978 && i < 986)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+		}
+		else if (i > 987 && i < 995)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 996 && i < 1004)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1005 && i < 1014)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1015 && i < 1024)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1058 && i < 1066)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+		}
+		else if (i > 1067 && i < 1075)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1076 && i < 1084)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1085 && i < 1094)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1095 && i < 1104)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1138 && i < 1146)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		
+		else if (i > 1156 && i < 1164)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		
+		else if (i > 1165 && i < 1174)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1175 && i < 1184)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		
+		else if (i > 1218 && i < 1244)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+		}
+		else if (i > 1245 && i < 1264)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1298 && i < 1324)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1325 && i < 1344)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+
+		}
+		else if (i > 1378 && i < 1404)
+		{
+			gotoxy(0,0);
+			textcolor(BLUE);
+			cprintf("%c",219);
+		}
+		else if (i > 1405 && i < 1424)
+		{
+    		gotoxy(0, 0);
+    		textcolor(BLUE);
+    		cprintf("%c", 219);
+		}
+		
+		else
+		{
+			gotoxy(0, 0);
+			textcolor(DARKGRAY);
+			cprintf("%c", 219);
+		}
+			
+	}
+	getch();
+}
